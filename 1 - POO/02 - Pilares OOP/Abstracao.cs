7ae5a066043914ec22b0eda08995169d22673723
eduardopasso <eduardo.pereira@tsea.com.br>
2025-04-15 14:38:15 -0300

    abstracao

---
+++ 1 - POO/02 - Pilares OOP/Abstracao.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP._1___POO._02___Pilares_OOP
+{
+    public abstract class EletroDocmestico 
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+
+        protected EletroDocmestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        public abstract void Ligar();
+
+        public abstract void Desligar();
+    }
+}
