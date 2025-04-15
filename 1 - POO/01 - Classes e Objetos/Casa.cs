dbee9424987f256218148fd864eb2760b69c459b
eduardopasso <eduardo.pereira@tsea.com.br>
2025-04-15 12:19:11 -0300

    Estado e Comportamento

---
+++ 1 - POO/01 - Classes e Objetos/Casa.cs
@@ -0,0 +1,30 @@
+﻿namespace OOP._1___POO._01___Classes_e_Objetos
+{
+    /// <summary>
+    /// Definição de Classe: Estrutura de código com a função de mapear um objeto do mundo real
+    /// </summary>
+    public class Casa
+    {
+        public int TamanhoM2 { get; set; }
+        public int Andares { get; set; }
+        public decimal Valor { get; set; }
+        public int NumeroVagas {get;set; }
+    }
+
+    /// <summary>
+    /// Definição de Objeto: Intanciação da Classe, ou seja, ela passa a estar representada na memória como um objeto
+    /// </summary>
+    public class Objeto
+    {
+        public Objeto()
+        {
+            var casa = new Casa()
+            {
+                TamanhoM2 = 100,
+                Andares = 2,
+                Valor = 100000,
+                NumeroVagas = 3,
+            };
+        }
+    }
+}
---
+++ 1 - POO/02 - Pilares OOP/EstadoEComportamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._1___POO._02___Pilares_OOP
+{
+    /// <summary>
+    /// O estado é representado pelas propriedades (atributos) da classe: Nome, Data Nascimento.
+    /// </summary>
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        /// <summary>
+        /// O comportamento é quando uma nova informação é processada através da classe: Transformar a DataNascimento em uma idade;
+        /// Comportamentos (métodos) podem alterar o valor do Estado da classe.
+        /// </summary>
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+            return idade;
+        }
+    }
+}
